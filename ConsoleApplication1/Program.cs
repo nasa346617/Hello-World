0365180763cb2701a72edb82c65711acfa08996d
nasa346617 <surayya.nasaruddin@exact.com>
2016-08-25 13:34:38 +0800

    edit comments

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-/*            "skjdnvhlivrekgvlxgfgbdtghrek" */
+/*            "skjdnvhlivrkshduvhuvihetghrek" */
 
         }
     }
